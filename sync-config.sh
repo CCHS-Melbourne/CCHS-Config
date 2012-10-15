db255e8bc01c953deac744a0cee8c87f10d806bd
John Spencer <john.spencer.9000@gmail.com>
2012-10-15 20:37:52 +1100

    fixed pronterface

--- sync-config.sh
+++ sync-config.sh
@@ -142,10 +142,10 @@ echo "...done";
 
 # Pronterface and Skeinforge
 echo -ne "Checking Pronterface and Skeinforge installed";
-if [ ! -e "/usr/bin/pronterface" ]; then
+if [ ! -d "/usr/share/pronterface" ]; then
 	apt-add-repository -y ppa:richi-paraeasy/ppa
 	apt-get -qq update
-	apt-get -qqy install printrun-gui skeinforge
+	apt-get -qqy install pronterface skeinforge
 fi
 echo "...done";
 
